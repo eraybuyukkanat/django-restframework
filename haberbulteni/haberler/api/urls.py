d3e247503c0a88e904aa36dc412064560b510064
Eray Büyükkanat <erybykknt@gmail.com>
2022-08-31 02:42:46 +0300

    added class based views

--- haberbulteni/haberler/api/urls.py
+++ haberbulteni/haberler/api/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path,include
 from haberler.api import views as api_views
 urlpatterns = [
-    path('makaleler/', api_views.makale_list_create_api_view,name='makale-listesi'),
-    path('makaleler/<int:pk>', api_views.makale_detail_api_view,name='makale-detay'),
+    path('makaleler/', api_views.MakaleListCreateAPIView.as_view(),name='makale-listesi'),
+    path('makaleler/<int:pk>', api_views.MakaleDetailAPIView.as_view(),name='makale-detay'),
 ]
+
+
+#FUNCTION BASED VIEWS URLS
+# urlpatterns = [
+#     path('makaleler/', api_views.makale_list_create_api_view,name='makale-listesi'),
+#     path('makaleler/<int:pk>', api_views.makale_detail_api_view,name='makale-detay'),
+# ]
